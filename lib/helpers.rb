05f04e610419ece994ea64f31474d47df8073657
James Healy <jimmy@deefa.com>
2009-04-09 23:00:35 +1000

    new helper method that might come in handy on day

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,9 @@ include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::XMLSitemap
+
+# Returns a sorted list of articles for the given year.
+# - borrowed from the stonesip.org codebase
+def articles_for_year(year)
+  @pages.select { |page| page.kind == 'article' and page.created_at.year == year }.sort_by { |page| page.created_at }.reverse
+end
